70816b667d42d12716b8d19375427488fcd0dd96
K8220 <K8220@OHJE-330-WS-06>
2017-03-23 14:55:40 +0200

    Class demo

---
+++ GameDemo/Flower.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace GameDemo
+{
+    public sealed partial class Flower : UserControl
+    {
+        public double locationX { get; set; }
+        public double locationY { get; set; }
+
+        public Flower()
+        {
+            this.InitializeComponent();
+
+            Width = Height = 50;
+        }
+
+        public void SetLocation()
+        {
+            SetValue(Canvas.LeftProperty, locationX);
+            SetValue(Canvas.TopProperty, locationY);
+        }
+    }
+}
